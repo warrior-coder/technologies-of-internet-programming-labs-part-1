78d09f9067d0e8d46521456b665f0286e42a7386
warrior-coder <79097093+warrior-coder@users.noreply.github.com>
2023-11-26 21:57:11 +0100

    -

--- Lab_3_7_GAS/lib/stat_pvn.js
+++ Lab_3_7_GAS/lib/stat_pvn.js
@@ -3,18 +3,13 @@
 
 // функция для вычисления среднего значения элементов массива
 function mean_arr(arr) {
-  return sum_arr(arr) / arr.length;
-}
-
-// функция для вычисления суммы элементов массива
-function sum_arr(arr) {
   var sum = 0;
 
   for (var i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
-
-  return sum;
+  
+  return sum / arr.length;
 }
 
 // функция для нахождения максимального значения элементов массива
